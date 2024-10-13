ccc958ed300bc1966e0902092f74e20407291e1c
piggypiggyyoinkyoink <quackquack904@gmail.com>
2024-10-13 15:08:42 +0100

    different ways of defining routes

--- experimentalisation.py
+++ experimentalisation.py
@@ -2,13 +2,27 @@ from fasthtml.common import *
 
 app,rt = fast_app()
 
-"""
+""" basic helo world
 @rt('/')
 def get(): return Div(P('Hello World!'), hx_get="/change")
 
 serve()
 """
+
+""" some html elements
 @app.get("/")
 def home():
     page = Html(Head(Title("Title")), Body(H1("This page is very nice"), Br(),P("Yes it is")))
     return page
+"""
+
+@app.get("/")
+def home():
+    return H1('Hello, World')
+
+@app.route("/", methods=['post', 'put'])
+def post_or_put():
+    return "got a POST or PUT request"
+@rt("/")
+def get():
+    pass
