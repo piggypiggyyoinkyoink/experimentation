e9c67f2792fda6f00b00e0aae84839ae1cee7e7f
piggypiggyyoinkyoink <quackquack904@gmail.com>
2024-10-13 16:02:57 +0100

    counter

--- experimentalisation.py
+++ experimentalisation.py
@@ -1,6 +1,6 @@
-from fasthtml.common import *
+"""from fasthtml.common import *
 
-app,rt = fast_app()
+app,rt = fast_app()"""
 
 """ basic helo world
 @rt('/')
@@ -27,3 +27,35 @@ def post_or_put():
 def get():
     pass
 """
+"""
+from fasthtml.common import *
+css = Style(':root {--pico-font-size:90%,--pico-font-family: Pacifico, cursive;}')
+app = FastHTML(hdrs=(picolink, css))
+
+@app.route("/")
+def get():
+    return (Title("Hello World"), 
+            Main(H1('Hello, World'), cls="container"))"""
+
+from fasthtml.common import *
+
+app = FastHTML()
+messages = ["This is a message, which will get rendered as a paragraph"]
+
+@app.get("/")
+def home():
+    return Main(H1('Messages'), 
+        *[P(msg) for msg in messages], #i have no idea why but removing the * breaks it
+        A("Link to Page 2 (to add messages)", href="/page2"))
+
+@app.get("/page2")
+def page2():
+    return Main(P("Add a message with the form below:"),
+                Form(Input(type="text", name="data"),
+                     Button("Submit"),
+                     action="/", method="post"))
+
+@app.post("/")
+def add_message(data:str):
+    messages.append(data)
+    return home()
