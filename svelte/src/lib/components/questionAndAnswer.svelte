1286bc5e4f6931392e3f79bcc8b56e28592b79dc
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-03-05 15:38:19 +0000

    passed properties and default properties to svelte components, including using the "spread" ... operator

--- svelte/src/lib/components/questionAndAnswer.svelte
+++ svelte/src/lib/components/questionAndAnswer.svelte
@@ -1,3 +1,12 @@
 <script>
-    let { question = "is Jack gay?", answer="yes"} = $props();
+    function getRandomAnswer(){
+        answers = ["Absolutely!", "Definitely", "Perhaps", "Let's see", "Not a chance", "Nope"]
+        return answers[Math.floor(Math.random()*answers.length)]
+    }
+    let answers = ["Absolutely!", "Definitely", "Perhaps", "Let's see", "Not a chance", "Nope"]
+    
+    let { question = "is Jack gay?", answer=getRandomAnswer()} = $props();
+    
 </script>
+
+<p> The answer to the question "{question}" is "{answer}".</p>
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script>
     import Hello from "$lib/components/hello.svelte"
+    import QuestionAndAnswer from "$lib/components/questionAndAnswer.svelte";
+    function getAnswer(){
+        let list = ["Yes", "Definitely", "Absolutely", "100%"]
+        return list[Math.floor(Math.random()*list.length)]
+    }
+    let qa = {
+        question: "Is Jack gay?",
+        answer: getAnswer()
+    };
 </script>
 
 <Hello />
+<QuestionAndAnswer {...qa}/>
