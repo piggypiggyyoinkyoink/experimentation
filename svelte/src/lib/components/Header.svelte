14e7a498aaf428d021a678c4decac61d163b24cf
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-03-11 15:41:56 +0000

    started new tutorial bc old one was bad

---
+++ svelte/src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+    let {name} = $props();
+</script>
+
+<h1>Hello {name}</h1>
+
+<style>
+
+</style>
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,18 +1,6 @@
 <script>
-    import Hello from "$lib/components/hello.svelte"
-    import QuestionAndAnswer from "$lib/components/questionAndAnswer.svelte";
-    import Counter from "$lib/components/counter.svelte";
-    function getAnswer(){
-        let list = ["Yes", "Definitely", "Absolutely", "100%"]
-        return list[Math.floor(Math.random()*list.length)]
-    }
-    let qa = {
-        question: "Is Jack gay?",
-        answer: getAnswer()
-    };
+    import Header from "$lib/components/Header.svelte" //convention is upper case
+    
 </script>
 
-<Hello />
-<Counter />
-<QuestionAndAnswer {...qa}/>
-<QuestionAndAnswer/>
+<Header name = "User"/>
