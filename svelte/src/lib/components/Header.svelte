e0287063bab49303ffce9856d3d4b477eb8998f5
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-03-11 16:15:18 +0000

    cool reactivity stuff with $state()

--- svelte/src/lib/components/Header.svelte
+++ svelte/src/lib/components/Header.svelte
@@ -1,9 +1,10 @@
 <script>
-    let {name} = $props();
+    let {name, fakeName=null} = $props(); //fakeName has a default value null, so is an optional parameter
 </script>
 
 <h1>Hello {name}</h1>
 <h2>{name.replaceAll("a", "o") }</h2>
+<p>{fakeName}</p>
 <style>
 
 </style>
