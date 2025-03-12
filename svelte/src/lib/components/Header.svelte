3d251556cccf238b84a58c2f6010d7069a1d6954
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-03-12 14:49:25 +0000

    loops

--- svelte/src/lib/components/Header.svelte
+++ svelte/src/lib/components/Header.svelte
@@ -1,18 +1,11 @@
 <script>
-    let {name, children, secondChild} = $props(); //any children passed into this component are assigned to "children" by default, unles they are named snippets, e.g. secondChild
+    let {name} = $props();
 </script>
 
 <div>
     
-    <h1>{#if name}{name}{:else}User{/if}'s Form'</h1>
-    {@render children()} <!-- rendering children passed to component -->
-    <h3>{@render secondChild("Jonathan")}</h3><!-- rendering named second child-->
+    <h1>{#if name}{name}{:else}User{/if}'s Form</h1>
 </div>
-<!--
-<h2>{name.replaceAll("a", "o") }</h2>
-
-<p>{fakeName}</p>
--->
 <style>
     div{
         background:coral;
--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -2,66 +2,76 @@
     import Header from "$lib/components/Header.svelte"; //convention is upper case
     
     let formState = $state({
-        name: "",
-        birthday:"",
+        answers:{},
         step:0,
         error:""
     });
+
+    const questions = [
+        {
+            question: "What is your name?",
+            id: "name",
+            type: "text"
+        },
+        {
+            question: "What's your birthday?",
+            id: "birthday",
+            type: "date"
+        },
+        {
+            question: "What's your favorite colour?",
+            id: "colour",
+            type: "color"
+        }
+    ]
+
+    function nextStep(id){
+        if (formState.answers[id]){
+            formState.step +=1;
+            formState.error="";
+        }else{
+            formState.error = "Please fill out the form ";
+        }
+    }
 </script>
 
-<Header name = {formState.name}>
-    <p>Hello :)</p> <!--passing children to a component-->
-    {#snippet secondChild(name)}
-        <p>Second Child {name}</p>
-    {/snippet}
-</Header> 
+<Header name = {formState.answers.name}/>
 <main>
+    {#if formState.step >= questions.length}
+    <p>Thank you</p>
+    {:else}
     <p> Step {formState.step + 1}</p>
-    
-<!--
-    {#if formState.step === 0}
-        <div>
-            <label for = "name">Your name</label>
-            <input type = "text" id = "name" bind:value={formState.name}>
-        </div>
-        <button onclick ={()=>{
-            if(formState.name !== ""){
-                formState.step += 1;
-                formState.error = "";
-            }else{
-                formState.error = "Please write your name."
-            }
-        }}>Next</button>
-    {:else if formState.step ===1}
-    <div>
-        <label for = "bday">Your birthday</label>
-        <input type = "date" id = "bday" bind:value={formState.birthday}>
-    </div>
-    <button onclick ={()=>{
-        if(formState.birthday !== ""){
-            formState.step += 1;
-            formState.error = "";
-        }else{
-            formState.error = "Please enter your birthday."
-        }
-    }}>Next</button>
     {/if}
--->
-    {@render formStep({question:"What is your name?", id:"name", type:"text"})}
+    {#each questions as question, index (question.id)} <!-- question.id is a 'key' here. It is not needed for everything, but recommended when whatever is beig iterated through is dynamic. Index refers to the index of the loop, can be called anything, e.g. i-->
+        {#if formState.step === index}
+            {@render formStep(question)}
+        {/if}
+    {/each}
+
+    <!-- Alternative:
+
+    {#each questions as {id, question, type} (id)}
+        {@render formStep({question, id, type})}
+    {/each}
+
+    -->
     {#if formState.error !==""}
         <p class="error">{formState.error}</p>
 
     {/if}
 </main>
 
+
 {#snippet formStep({question,id,type})}
-    <article>
-        <div>
-            <label for = {id}>{question}</label>
-            <input {type} id = {id} bind:value={formState[id]}/>
-        </div>
-    </article>
+<article>
+    <div>
+        <label for = {id}>{question}</label>
+        <input {type} id = {id} bind:value={formState.answers[id]}/> <!--This creates a dictionary with key being the id for each field, and the value being the user inputted value-->
+    </div>
+    <button onclick={()=>nextStep(id)}>Next</button> <!--Next button calls nextStep function with current question id-->
+</article>
 {/snippet}
+<!--{JSON.stringify(formState)} <!--To visualise what the above block is doing-->
 
 <style> /*Note: the scope of these CSS styles is restricted to this component, not other components e.g. Header that are imported by it. Likewise, the scope is restricted to this component when other components import this one.*/
     div{
