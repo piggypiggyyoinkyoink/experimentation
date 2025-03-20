5f94849f90a4c95fe342242aa6105d8d16a48422
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-03-20 16:24:03 +0000

    defining and updating states using a separate file and exporting

--- svelte/src/lib/components/Header.svelte
+++ svelte/src/lib/components/Header.svelte
@@ -1,10 +1,16 @@
 <script>
+    import {createState} from "$lib/modules/state.svelte"
     let {name} = $props();
+
+    const myState = createState();
+
+
 </script>
 
 <div>
     
     <h1>{#if name}{name}{:else}User{/if}'s Form</h1>
+    <button onclick = {myState.up}>{myState.value}</button>
 </div>
 <style>
     div{
---
+++ svelte/src/lib/modules/state.svelte.js
@@ -0,0 +1,19 @@
+export function createState(){
+    let value = $state(0);
+    function up(){
+        value ++;
+    }
+    return {
+        //this returns getter and setter methods, as well as any other functions (i.e. up) corresponding to the reactive variable value(), which can then be accessed by any components importing this module.
+        // If you dont want the value to be modified by external code directly, you can omit the set() function.
+
+        //(this can be seen as being too much effort for what its worth)
+        get value(){ 
+            return value;
+        },
+        set value(newValue){
+            value = newValue;
+        },
+        up
+    };
+}
